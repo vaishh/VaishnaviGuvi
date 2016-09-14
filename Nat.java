af834edb094f8854ee3166f2eb529823bf2721bb
vaishh <rvaishnavikrr@gmail.com>
2016-09-14 09:13:36 +0530

    Create Nat.java

---
+++ Nat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Nat {
+
+	public static void main(String[] args) {
+	
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int sum;
+sum=a*(a+1)/2;
+System.out.println(sum);
+	}
+
