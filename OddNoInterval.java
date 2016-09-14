1e1fe505f9b80e7239d8c2158efa3095231b5747
vaishh <rvaishnavikrr@gmail.com>
2016-09-14 09:38:15 +0530

    Create OddNoInterval.java

---
+++ OddNoInterval.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class OddNoInterval {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+for(int i=a;i<=b;i++)
+{
+	if(i%2!=0)
+	{
+		System.out.println(i);
+	}
+}
+
+	}
+
+}
