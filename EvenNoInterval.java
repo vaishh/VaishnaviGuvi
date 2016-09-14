58d94e46fb86ac6d86603457168f24b33defe121
vaishh <rvaishnavikrr@gmail.com>
2016-09-14 09:41:04 +0530

    Create EvenNoInterval.java

---
+++ EvenNoInterval.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class EvenNoInterval {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+for(int i=a;i<=b;i++)
+{
+	if(i%2==0)
+	{
+		System.out.println(i);
+	}
+}
+
+	}
+
+}
