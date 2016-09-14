d0447aeeb5fbe6ef210570197f7d1d19a5eb14e5
vaishh <rvaishnavikrr@gmail.com>
2016-09-14 09:18:12 +0530

    Create NoOfDigits.java

---
+++ NoOfDigits.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class NoOfDigits
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+String s=String.valueOf(n);
+int n=s.length();
+System.out.println(n);
+}
+}
